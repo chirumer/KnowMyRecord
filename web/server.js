ad39b3bad252de16a85b47f1c4f366af65980a89
chirumer <imchirumer@gmail.com>
2022-12-11 14:58:12 +0530

    ensure env variables are loaded

---
+++ web/server.js
@@ -0,0 +1,23 @@
+// load environment variables from .env file
+require('dotenv').config()
+
+// verify the required environment variables are configured
+function verify_env_defined(name) {
+  if (process.env[name] == undefined) {
+    console.error(`Environment Variable [${name}] is required to be defined.`);
+    process.exit(1);
+  }
+}
+required_env_variables = ['PORT', 'NODE_ENV', 'TOKEN_SECRET']
+required_env_variables.forEach(env_var => {
+  verify_env_defined(env_var);
+});
+
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT
+
+app.listen(port, () => {
+  console.log(`Listening On ${PORT}`)
+})
